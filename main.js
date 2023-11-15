08d78c837b02b723de3c60752c4dff2de3c5b155
Gooper100000 <142946620+Gooper100000@users.noreply.github.com>
2023-11-15 17:35:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var first_name= "Raghav"
+var last_name= "Gupta"
+console.log(first_name)
+console.log(last_name)
+var full_name= first_name.concat(last_name)
+console.log (full_name)
+var len= full_name.length
+console.log (len)
+var replace_name=full_name.replace("Raghav","Siara")
+console.log(replace_name)
+var caps=full_name.toUpperCase()
+console.log(caps)
+var lower=full_name.toLowerCase()
+console.log(lower)
+var sqr=Math.sqrt();
+console.log(sqr)
+var maximum_number=Math.max(15, 20)
+console.log(maximum_number)
+var minimum_number=Math.min(15, 20)
+console.log(minimum_number)
+var power_eg_1=Math.pow(2,2)
+console.log(power_eg_1)
+var a =10
+var b =5
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
